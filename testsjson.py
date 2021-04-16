2e97a44f21925f56fc86a268c74e73667c8f6a56
silanoc <silanoc@gmail.com>
2021-04-16 14:23:27 +0200

    pourquoi ça marche maintenant ???

--- testsjson.py
+++ testsjson.py
@@ -1,33 +1,25 @@
 import json
 
-"""
-class Agent:
 
-    def __init__(self, position, **agent_attributes):
-        self.position = position
-        for attr_name, attr_value in agent_attributes.items():
+class Perso:   
+    
+    def __init__(self,**tous_les_monstres):
+        for attr_name, attr_value in tous_les_monstres.items():
             setattr(self, attr_name, attr_value)
+            #
+            # self.pvencours=self.pvmax
 
-for agent_attributes in json.load(open("agents-100k.json")):
-        latitude = agent_attributes.pop("latitude")
-        longitude = agent_attributes.pop("longitude")
-        position = Position(latitude, longitude)
-        agent = Agent(position, **agent_attributes)
-        print(agent.position.longitude)
-        print(agent.position.latitude)
-"""
 
-class Perso:
-    def __init__(self, **caract_monstre):
-        for attr_name, attr_value in caract_monstre.items():
-            setattr(self, attr_name, attr_value)
-        
 
-#for caract_monstre in json.load(open("/home/gabriel-le/Téléchargements/la_poo_avec_python-00_setup/agents-100k.json")):
-for caract_monstre in json.load(open("data_monstre.json")):
-    monstre=Perso(**caract_monstre)
+def main():
+    for tous_les_monstres in json.load(open("data_monstre.json")):
+        monstre = Perso(**tous_les_monstres)
+        print(tous_les_monstres)
+        print(monstre)
+        print(monstre.classe)
+        print(monstre.pvmax)
+        print(monstre.pvencours)
+        print("---")
 
-print(caract_monstre)
-print(monstre.classe)
-print(monstre.pvmax)
-#print(monstre.sex)
+main()
+
