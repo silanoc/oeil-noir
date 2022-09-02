428261ae1fe2bdf9de406421503566107afaa030
silanoc <silanoc@gmail.com>
2022-09-02 19:37:51 +0200

    TDD red, ajout test est vivant pour pouvoir modifier le code

--- tests/test_combatentremonstre.py
+++ tests/test_combatentremonstre.py
@@ -1,12 +1,13 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
-import pytest
-import os
+#import pytest
+#import os
 
 # Ligne nécessaire pour certaines configurations
-pytest.main(args=['-s', os.path.abspath('test_combatentremonstre.py')])
+#pytest.main(args=['-s', os.path.abspath('test_combatentremonstre.py')])
 
+#import programme.combatentremonstre as combatentremonstre
 import programme.combatentremonstre as combatentremonstre
 
 """Test combat entre monstre"""
@@ -14,6 +15,16 @@ import programme.combatentremonstre as combatentremonstre
 class Test_combatentremonstre():
     
     def test_existance_monstre(self):
-        #monstre = combatentremonstre.Perso()
-        #assert type(monstre.pvencours) is int 
-        pass
+        monstre = combatentremonstre.Perso(pvmax = 10, classe = "ork")
+        assert type(monstre.pvencours) is int and monstre.classe == "ork"
+        
+    def test_est_vivant(self):
+        monstre1 = combatentremonstre.Perso(pvmax = 10, classe = "ork")
+        monstre1.pvencours = 0
+        assert monstre1.est_vivant() == False 
+        monstre2 = combatentremonstre.Perso(pvmax = 10, classe = "ork")
+        monstre2.pvencours = 0
+        assert monstre2.est_vivant() == True 
+
+        
+        
