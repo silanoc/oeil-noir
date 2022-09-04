a93af94b75dd4f342ee4a0c55b1b66b37a8db393
silanoc <silanoc@gmail.com>
2022-09-04 18:20:23 +0200

    test et refactrorisation de Combat

--- domaine/combat.py
+++ domaine/combat.py
@@ -5,64 +5,83 @@ from domaine.lanceur_de import d6, d20
 from domaine.entite import Entite as Entite
 
 class Combat():
+    """Defini l'attaquant et le défenseur dans un combat.
+    Classe permattant d'effectuer le combat entre les deux.
+        
+    :param Entite joueur1: une instance de Entite
+    :param Entite joueur2: une instance de Entite
+    """
     
     def __init__(self,joueur1: Entite, joueur2: Entite) -> None:
-        """Defini l'attaquant et le défenseur dans un combat.
-        
-        :param Entite joueur1: une instance de Entite
-        :param Entite joueur2: une instance de Entite"""
         self.attaquant = joueur1
         self.defenseur = joueur2
     
-    def determine_degat(self,nbdedegat: int ,valbonus: int, chiffre_protection: int) -> int:
-        """défini le nmbre de point de dégat du type 2d6+2 - protection d'armure
-        soit [nbdedegat]d6+[valbonus] - [chiffre_protection]
+    def determine_degat(self,nbdedegat: int ,valbonus: int, value_protection: int) -> int:
+        """défini le nombre de point de dégat du type 2d6+2 - protection d'armure
+        soit [nbdedegat]d6+[valbonus] - [value_protection]
         Ne peux pas être négatif
         
+        :param int nbdedegat: nombre de dé de dégat.
+        :param int valbonus: nombre de dégat bonus, peut etre négatif.
+        :param int value_protection: valeur de la protection. 
+            Nbre positif, doit être soustrait ou passé en négatif pour addition.
+        
+        :returns: totaldegat 
+        :rtype: int
         """
         degat: list[int] = [d6() for i in range(nbdedegat)]
-        complement: list[int] = [valbonus, chiffre_protection * -1 ]
+        complement: list[int] = [valbonus, value_protection * -1 ]
         degat.extend(complement)
         totaldegat: int = sum(degat)
         if totaldegat < 0 : totaldegat = 0
         return totaldegat
-        
-        """
-        degat: int = 0
-        for i in range (nbdedegat):
-            degat += d6()
-        degat = degat + valbonus - chiffre_protection
-        if degat < 0:
-            degat = 0
-        return degat
-        """
     
     def effectue_combat(self) -> Entite:
+        """Le combat est constitué d'assaut. Un assaut de 2 phases.
+        A chaque phase on alterne qui attaque et qui pare.
+        Le combat s'arrete quand un des deux n'a plus de point de vie.
+        
+        :returns: gagnant, qui des deux protagonistes à gagné le combat.
+        :rtype: Entite
+        
+        """
         self.attaquant.pvencours: int = self.attaquant.pvmax
         self.defenseur.pvencours: int = self.defenseur.pvmax
-        tour_combat: int = 0  
-        while ((self.attaquant.pvencours >= 0) and (self.defenseur.pvencours >= 0)):
-        #while self.attaquant.est_vivant() and self.defenseur.est_vivant():
-            tour_combat += 1
-            #print("tour :" + str(tour_combat))
-            att = self.attaquant.jet_attaque(d20())
-            if att == True:
-                defe = self.defenseur.jet_parade(d20())
-                if defe == False:
-                    self.defenseur.pvencours -= self.determine_degat(self.attaquant.nb_de_degat, self.attaquant.degat_bonus,self.defenseur.valeur_protection)
-                    #print("def pv : " + str(self.defenseur.pvencours))
-                    if self.defenseur.pvencours <= 0:
-                    #if not self.defenseur.est_vivant:
-                        gagnant = self.attaquant
-                        return gagnant
-                        break
-            att = self.defenseur.jet_attaque(d20())
-            if att == True:
-                defe = self.attaquant.jet_parade(d20())
-                if defe == False:
-                    self.attaquant.pvencours -= self.determine_degat(self.defenseur.nb_de_degat, self.defenseur.degat_bonus,self.attaquant.valeur_protection)
-                    #print ("att pv : "+str(self.attaquant.pvencours))
-                    if self.attaquant.pvencours <= 0:
-                        gagnant = self.defenseur
-                        return gagnant                  
-                        break                
+        nb_assaut: int = 0
+        phase:int = 1
+        gagnant = None
+        #while self.attaquant.pvencours >= 0 and self.defenseur.pvencours >= 0:
+        while self.attaquant.est_vivant and self.defenseur.est_vivant:
+            nb_assaut += 1
+            #print("Assaut numéro :" + str(nb_assaut))
+            if phase == 1:
+                self.phase_combat(celui_qui_attaque = self.attaquant, celui_qui_pare = self.defenseur)
+                phase: int = 2    
+            elif phase == 2:
+                self.phase_combat(celui_qui_attaque = self.defenseur, celui_qui_pare = self.attaquant)
+                phase: int = 1
+
+            if self.attaquant.pvencours < 0:
+                gagnant: Entite =  self.defenseur
+                break    
+            if self.defenseur.pvencours < 0:
+                gagnant: Entite = self.attaquant
+                break      
+        return gagnant
+            
+    def phase_combat(self, celui_qui_attaque: Entite, celui_qui_pare: Entite):
+        """1 phase :
+        La personne qui attaque doit faire un test d'attaque.
+        Si loupé, fin.
+        Si réussit, le défenseur doit testé sa parade.
+        Si la parade est réussi, aucun dégat, sinon calculer retirer les dégats des pv en cours.
+        
+        :param Entite celui_qui_attaque:
+        :param Entite celui_qui_pare"""
+        att: bool =  celui_qui_attaque.jet_attaque(d20())
+        if att == True:
+            defe: bool =  celui_qui_pare.jet_parade(d20())
+            if defe == False:
+                celui_qui_pare.pvencours -= self.determine_degat(celui_qui_attaque.nb_de_degat,
+                                                                      celui_qui_attaque.degat_bonus,
+                                                                      celui_qui_attaque.valeur_protection)      
--- interface/tests/test_combat.py
+++ interface/tests/test_combat.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 from domaine.combat import Combat
+from domaine.entite import Entite
 
 class Test_combat():
     
@@ -13,6 +14,13 @@ class Test_combat():
         petit_combat = Combat("Dieu", "Monstre en bois")
         for i in range (100):
             assert 1 <= petit_combat.determine_degat(1,1,1) <= 6
+            
+    def test_effectue_combat(self):
+        dieu = Entite(classe = "Dieu", pvmax = 999, attaque = 20, parade = 20, nb_de_degat = 5, degat_bonus = 10, valeur_protection = 10)
+        monstreenbois = Entite(classe = "Monstre en bois", pvmax = 1, attaque = 1, parade = 1, nb_de_degat = 1, degat_bonus = 1, valeur_protection = 1)
+        petit_combat = Combat(dieu, monstreenbois)        
+        resultat = petit_combat.effectue_combat()
+        assert resultat == dieu
         
         
         
